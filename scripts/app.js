7825f1b5482f601dfd57ba9677ed584c80b5b99a
Sir-Lauty <149327070+Sir-Lauty@users.noreply.github.com>
2023-11-26 12:23:49 -0300

    a

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,1 @@
-const c_imgs = document.querySelector('.c_imgs');
-const c_points = document.querySelector('.c_points');
 
-c_points.forEach((cadaPunto,i)=>{
-    c_points[i].addEventListener('click',()=>{
-        let posicion = i;
-        let operacion = posicion * -50;
-        c_imgs.computedStyleMap.transform = 'translateX(${operacion}%)'
-        c_points.forEach((cadaPunto,i)=>{
-            c_points[i].classList.remove('active')
-        })
-        c_points[i].classList.add('active')
-    })
-})
